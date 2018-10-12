0edbf20fa2ba9d4bdfc0afd60fea372a8c040b7a
hamadniaz <hamadniaz007@gmail.com>
2018-10-12 13:42:37 +0500

    Enduro Boilerplate Code

---
+++ cms/index.js
@@ -0,0 +1,24 @@
+({
+	first_slide: {
+		header: 'Woooooow!',
+		tagline: 'Your first enduro.js project',
+		call_to_action: 'Scroll down to see what\'s up'
+	},
+	instruction_points: [
+		{
+			header: 'Admin interface',
+			tagline: 'Create amazing content with ease',
+			link: 'https://www.endurojs.com/docs/introduction-to-admin'
+		}
+	],
+	so_much_more_block: {
+		header: 'Sooooooo',
+		tagline: 'much more',
+		list_of_topics: [
+			{
+				tagline: 'Host your page on netlify:',
+				link: 'http://www.endurojs.com/docs/netlify'
+			}
+		]
+	}
+})
